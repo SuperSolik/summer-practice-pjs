26b50f076ecc4f84ccd113ab6e200b0d6cd19294
SuperSolik <serpetr99@gmail.com>
2019-07-05 16:53:21 +0300

    added stack-based dfs

--- src/graph/GraphAlgo.java
+++ src/graph/GraphAlgo.java
@@ -2,6 +2,7 @@ package graph;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Stack;
 
 public class GraphAlgo {
 
@@ -17,22 +18,33 @@ public class GraphAlgo {
     }
 
     public void DFS(Graph graph, JTable list) {
-        Integer time = new Integer(0);
+        int time = 0;
         for (Vertex v : graph.getVertices()) {
             if (v.getColor() == Color.WHITE) {
                 visit(v, list, time);
             }
         }
+        for(Vertex v : graph.getVertices()){
+            System.out.println(v.getColor() == Color.BLACK);
+        }
     }
 
-    private void visit(Vertex v, JTable list, Integer time) {
-        for (Edge e : v.getEdges()) {
-            Vertex temp = e.getDest();
-            if (temp.getColor() == Color.WHITE) {
-                visit(temp, list, time);
+    private void visit(Vertex v, JTable list, int time) {
+        Stack<Vertex> stack = new Stack<>();
+        Vertex current;
+        stack.push(v);
+        while(!stack.empty()) {
+            current = stack.pop();
+            if(current.getColor() == Color.WHITE){
+                current.setColor(Color.BLACK);
+            }
+
+            for (Edge e : v.getEdges()) {
+                Vertex temp = e.getDest();
+                if (temp.getColor() == Color.WHITE) {
+                    stack.push(temp);
+                }
             }
         }
-        v.setColor(Color.BLACK);
-        //TODO записать time
     }
 }
