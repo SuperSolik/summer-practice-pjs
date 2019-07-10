c3bd33c49776ad37e89bdb2e24f82c529c21bd01
SuperSolik <serpetr99@gmail.com>
2019-07-11 02:21:20 +0300

    graph generating for tests

--- src/graph/GraphAlgoTest.java
+++ src/graph/GraphAlgoTest.java
@@ -5,6 +5,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.Random;
+
 public class GraphAlgoTest extends TestCase {
     private Graph g;
     private GraphAlgo algo;
@@ -13,16 +15,29 @@ public class GraphAlgoTest extends TestCase {
     public void setUp() throws Exception {
         g = new Graph();
         algo = new GraphAlgo();
+        g.onModify(() -> {});
+        Random r = new Random();
+        int min = 5;
+        int max = 100;
+        int vert = min + r.nextInt(max-min+1);
+        int from, to;
+        for(int i = 0; i < vert; i++)
+            g.createVertex(String.valueOf(i));
+
+        for(int i = 0; i < vert; i++){
+            from = r.nextInt(vert);
+            to = r.nextInt(vert);
+            g.createEdge(String.valueOf(from), String.valueOf(to));
+        }
+        System.out.println(g);
     }
 
     @After
     public void tearDown() throws Exception {
-        g = null;
-        algo = null;
     }
 
     @Test
     public void testKosaraju() {
-        assertEquals(g.getVertices().size(), 0);
+
     }
 }
